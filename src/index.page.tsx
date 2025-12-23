d01fb90c4f0227b7b9bf65385dea926cba6dde12
RyosukeKubota <sukemaro_0@yahoo.co.jp>
2025-12-23 21:45:11 +0900

    update

--- src/index.page.tsx
+++ src/index.page.tsx
@@ -1,8 +1,37 @@
+import dayjs from "dayjs";
+
 export const title = "sukemaru|aboutme";
 export const layout = "_includes/layout.tsx";
 
-export default ({ comp, works }: Lume.Data, _helpers: Lume.Helpers) => {
-  console.log(works);
+const WorkView = (props: {
+  title: string;
+  description: string;
+  url: string;
+  startDate: string;
+  endDate: string;
+  techs: string;
+}) => {
+  const formattedStartDate = dayjs(props.startDate).format("YYYY.MM");
+  const formattedEndDate = props.endDate
+    ? dayjs(props.endDate).format("YYYY.MM")
+    : "現在";
+  return (
+    <div class="work-row">
+      <div class="work-head">
+        <h3>{props.title}</h3>
+        <p class="work-dates">{formattedStartDate} - {formattedEndDate}</p>
+      </div>
+      <p class="work-description">
+        {props.description}
+      </p>
+    </div>
+  );
+};
+
+export default (
+  { comp, works: { list } }: Lume.Data,
+  _helpers: Lume.Helpers,
+) => {
   return (
     <div class="page-wrapper">
       <section class="profile">
@@ -14,7 +43,7 @@ export default ({ comp, works }: Lume.Data, _helpers: Lume.Helpers) => {
 
       <section class="basic-section">
         <comp.Title>
-          Works
+          個人開発
         </comp.Title>
         <div>
           - 色々なWebサイト制作・運用<br />
@@ -24,11 +53,17 @@ export default ({ comp, works }: Lume.Data, _helpers: Lume.Helpers) => {
 
       <section class="basic-section">
         <comp.Title>
-          個人開発
+          Works
         </comp.Title>
-        <div>
-          - 色々なWebサイト制作・運用<br />
-          - ブログ執筆
+        <div class="works-section">
+          {list.map((work: {
+            title: string;
+            description: string;
+            url: string;
+            startDate: string;
+            endDate: string;
+            techs: string;
+          }) => <WorkView {...work} />)}
         </div>
       </section>
     </div>
